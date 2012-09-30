20a3a3200a33a97aad914fb85e720086def3a76e
John O'Neill <jconeill@princeton.edu>
2012-09-30 06:33:15 -0400

    plotted multiple sources

--- static/global.js
+++ static/global.js
@@ -3,11 +3,11 @@ var EMBED_KEY = 'd8c645e9b9d643b49d1e0ccb3f66e89e';
 $(function() {
 	// Prefill the Query with Gangnam style
 	$('#queryForm .query').val('http://www.youtube.com/watch?v=9bZkp7q19f0');
-	
+
 	// Query
 	$('#queryForm').bind('submit', function() {
 		var query = $(this).find('.query').val();
-		
+
 		$.get('http://api.embed.ly/1/oembed?key=' + EMBED_KEY + '&url=' + query, function(data) {
 			if (data && data.thumbnail_url) {
 				$('body').css('background', '#424242 url(\'' + data.thumbnail_url + '\') repeat');
@@ -16,7 +16,8 @@ $(function() {
 		});
 
         getData(query,  function(data) {
-            graph_lines(data.x, data.y);
+console.log(data);
+            graph_lines(data);
             return data;
         });
 
@@ -28,7 +29,7 @@ function getData(url, fn) {
     $.getJSON('/data/' + encodeURIComponent(url), fn);
 }
 
-function graph_lines(x, y) {
+function graph_lines(data) {
 	var chart;
 	$(document).ready(function() {
 	    chart = new Highcharts.Chart({
@@ -39,15 +40,15 @@ function graph_lines(x, y) {
 	            marginBottom: 25
 	        },
 	        title: {
-	            text: 'Title',
+	            text: 'Total',
 	            x: -20 //center
 	        },
 	        subtitle: {
-	            text: 'Subtitle',
+	            text: 'Aggregate views',
 	            x: -20
 	        },
 	        xAxis: {
-	            categories: x
+	            categories: data.x
 	        },
 	        yAxis: {
 	            title: {
@@ -75,8 +76,10 @@ function graph_lines(x, y) {
 	        },
 	        series: [{
 	            name: 'Domain 1',
-	            data: y
-	        }]
+				data: data.y
+			}, {name: 'Domain 2',
+				data: data.z
+			}]
 	    });
 	});
-}	
+}
