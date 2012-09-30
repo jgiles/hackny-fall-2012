d3cc1993d91ce2646c3e02167b729856d5a8e06c
David Lackey <davidmlackey@me.com>
2012-09-30 04:23:50 -0400

    added tiled bg

--- static/global.js
+++ static/global.js
@@ -19,30 +19,16 @@ $(function() {
  	  	});
 	});
 	
-	// Bubble visualization
-	initBubbles();
-
+	// Prefill the Query with Gangnam style
+	$('#queryForm .query').val('http://www.youtube.com/watch?v=9bZkp7q19f0');
 	
-	// Query
+	// Bind query submission and trigger submit
 	$('#queryForm').bind('submit', function() {
 		var query = $(this).find('.query').val();
-
-
-		
-		//$('#embedWrapper').html('<a href="' + query + '"></a>')
-		//	.embedly({key: EMBED_KEY});
-		
 		$.get('http://api.embed.ly/1/oembed?key=' + EMBED_KEY + '&url=' + query, function(data) {
-			console.log(data);
 			if (data && data.thumbnail_url) {
-				if (data.html) {
-					$('#embedHtml').html(data.html).show();
-					$('#embedPic').hide();
-				}
-				else if(data.thumbnail_url) {
-					$('#embedPic').attr('src', data.thumbnail_url).show();
-					$('#embedHtml').hide();
-				}
+				$('body').css('background', '#424242 url(\'' + data.thumbnail_url + '\') repeat');
+
 			}
 		});
         
@@ -52,60 +38,8 @@ $(function() {
         });
 		
 		return false;
-	});
+	}).trigger('submit');
 });
-
-
-
-function initBubbles() {
-	var r = 960,
-	    format = d3.format(",d"),
-	    fill = d3.scale.category20c();
-	
-	var bubble = d3.layout.pack()
-	    .sort(null)
-	    .size([r, r])
-	    .padding(1.5);
-	
-	var vis = d3.select("#chart").append("svg")
-	    .attr("width", r)
-	    .attr("height", r)
-	    .attr("class", "bubble");
-	
-	d3.json("/static/flare.json", function(json) {
-	  var node = vis.selectAll("g.node")
-	      .data(bubble.nodes(classes(json))
-	      .filter(function(d) { return !d.children; }))
-	    .enter().append("g")
-	      .attr("class", "node")
-	      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	
-	  node.append("title")
-	      .text(function(d) { return d.className + ": " + format(d.value); });
-	
-	  node.append("circle")
-	      .attr("r", function(d) { return d.r; })
-	      .style("fill", function(d) { return fill(d.packageName); });
-	
-	  node.append("text")
-	      .attr("text-anchor", "middle")
-	      .attr("dy", ".3em")
-	      .text(function(d) { return d.className.substring(0, d.r / 3); });
-	});
-	
-	// Returns a flattened hierarchy containing all leaf nodes under the root.
-	function classes(root) {
-	  var classes = [];
-	
-	  function recurse(name, node) {
-	    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-	    else classes.push({packageName: name, className: node.name, value: node.size});
-	  }
-	
-	  recurse(null, root);
-	  return {children: classes};
-	}
-}
 
 function getData(url, fn) {
     $.getJSON('/data/' + encodeURIComponent(url), fn);
