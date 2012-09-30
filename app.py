af4e39443ec56969c236fff6b31074d98bf77d6c
Josh Giles <jgiles17@gmail.com>
2012-09-30 03:22:49 -0400

    fixed typo

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 @app.route('/data/<path:memeurl>')
 def josh(memeurl):
     shortlink = getBitlyShortURL(memeurl)
-    history = getURLClickHistory(shortlink, unit='day', units=7)
+    history = getURLClickHistory(shortlink, unit='day', units=10)
     return Response(dumps(history), mimetype='application/json')
 
 if __name__ == '__main__':
