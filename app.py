a0861f9296aea6880cb4b89a3567d9976c7eda2b
Josh Giles <jgiles17@gmail.com>
2012-09-29 23:03:54 -0400

    Added json endpoint taking urls.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, Response
+from json import dumps
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/data/<path:memeurl>')
+def josh(memeurl):
+    return Response(dumps({'arg': memeurl}), mimetype='application/json')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
