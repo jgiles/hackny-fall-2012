2d97270f967dff247516fbc0f702578ebeeb6f49
Josh Giles <jgiles17@gmail.com>
2012-09-30 04:51:53 -0400

    Random data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, Response, render_template
 from json import dumps
 from bitly.bitly_client import *
+from random import random
 
 app = Flask(__name__)
 
@@ -11,20 +12,24 @@ def index():
 
 @app.route('/data/<path:memeurl>')
 def data(memeurl):
+    units = 365
     rep = getBitlyRep(memeurl)
     
     created = getBitlyCreated(rep['hash'], rep['short_url'])
-    history = getURLClickHistory(rep['short_url'], unit='day', units=100)
-    
+    history = getURLClickHistory(rep['short_url'], unit='day', units=units)
+    referrals = getReferringDomains(rep['short_url'], unit='day', units=units)
+    # return Response(dumps(referrals), mimetype = 'application/json')
     clicks = []
     time = []
+    z = []
     for clickdata in history['data']['link_clicks']:
         if clickdata['dt'] < created:
             continue
         clicks.append(clickdata['clicks'])
         time.append(clickdata['dt'])
+        z.append(clickdata['clicks']*(.8 + .4*random()))
 
-    return Response(dumps({'x':time, 'y':clicks, 'created':created}), mimetype='application/json')
+    return Response(dumps({'x':time, 'y':clicks, 'z':z, 'created':created}), mimetype='application/json')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
