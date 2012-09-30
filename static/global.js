6fb70c1967774e47ccb920a348875e6fe5d8ed6e
David Lackey <davidmlackey@me.com>
2012-09-30 01:04:19 -0400

    everything

--- static/global.js
+++ static/global.js
@@ -1,17 +1,26 @@
 $(function() {
+	// Timeline
+	var topDiff = 3;
 
 	// Move to first day
-       var firstEl = $('.timeline li:first');
-       $('.timeline .slider').css('top', firstEl.position().top);
+    var firstEl = $('.timeline li:first');
+    $('.timeline .slider').css('top', firstEl.position().top - topDiff);
 
 	// Move to clicked day
 	$('.timeline li').bind('click', function() {
 		console.log("test");
-   var el = $(this);
-   $('.timeline .slider').animate({
-	   top: el.position().top
-   }, 500, function() {
-      // Animation complete.
-   });
-});
+   		var el = $(this);
+   		$('.timeline .slider').animate({
+	   		top: el.position().top - topDiff
+   		}, 350, function() {
+      		// Animation complete.
+ 	  	});
+	});
+	
+	
+	// Query
+	$('#queryForm').bind('submit', function() {
+		alert('hey cunt');
+		return false;
+	});
 });
